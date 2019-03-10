bccfdf4824dcb4c5fedabe7ca567ffa585616ea0
hkloudou <hkloudou@gmail.com>
2019-03-11 04:56:48 +0800

    fix

--- sdk/golang/sample/main.go
+++ sdk/golang/sample/main.go
@@ -18,11 +18,27 @@ func init() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	//defer conn.Close()
-	//c := webfontsdk.NewGreeterClient(conn)
 }
 
-func main() {
+func fontList() {
+	c := webfontsdk.NewGreeterClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	req := &webfontsdk.FontListRequest{
+		Apikey: "5873b2bc-84e7-490b-b637-58aaf7d59240",
+	}
+	if ret, err := c.FontList(ctx, req); err != nil {
+		if actual, ok := status.FromError(err); ok {
+			log.Println("actual", "code", actual.Code(), "err:", actual.Message())
+		}
+	} else {
+		for k, v := range ret.Fonts {
+			log.Println("list", k, v)
+		}
+	}
+}
+
+func fontBuild() {
 	c := webfontsdk.NewGreeterClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -34,7 +50,6 @@ func main() {
 		NeedEot:  true,
 		NeedWoff: true,
 	}
-
 	if ret, err := c.BuildFont(ctx, req); err != nil {
 		if actual, ok := status.FromError(err); ok {
 			log.Println("actual", "code", actual.Code(), "err:", actual.Message())
@@ -53,5 +68,54 @@ func main() {
 		}
 		//拼装font-familly
 	}
+}
+
+func multfontBuild() {
+	c := webfontsdk.NewGreeterClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	req := &webfontsdk.MultFontBuildRequest{}
+	req.Items = make([]*webfontsdk.FontBuildRequest, 0)
+	req.Items = append(req.Items, &webfontsdk.FontBuildRequest{
+		FontId:   45803,
+		Apikey:   "5873b2bc-84e7-490b-b637-58aaf7d59240",
+		Text:     "苏打粉地方",
+		NeedTtf:  true,
+		NeedEot:  true,
+		NeedWoff: true,
+	})
+
+	req.Items = append(req.Items, &webfontsdk.FontBuildRequest{
+		FontId:   168,
+		Apikey:   "5873b2bc-84e7-490b-b637-58aaf7d59240",
+		Text:     "测试",
+		NeedTtf:  true,
+		NeedEot:  true,
+		NeedWoff: true,
+	})
+	if rets, err := c.MultBuildFont(ctx, req); err != nil {
+		if actual, ok := status.FromError(err); ok {
+			log.Println("actual", "code", actual.Code(), "err:", actual.Message())
+		}
+	} else {
+		for _, ret := range rets.Items {
+
+			if len(ret.GetBytesTtf()) > 0 {
+				//savettf
+			}
+
+			if len(ret.GetBytesEot()) > 0 {
+				//saveeot
+			}
+
+			if len(ret.GetBytesWoff()) > 0 {
+				//savewoff
+			}
+			//拼装font-familly
+		}
+	}
+}
+
+func main() {
 
 }
