bdb21956df9e3551c55c0503e38136f1272999bf
hkloudou <hkloudou@gmail.com>
2019-03-11 02:15:33 +0800

    fix

--- sdk/golang/webfontsdk/webfont.pb_test.go
+++ sdk/golang/webfontsdk/webfont.pb_test.go
@@ -1,54 +1,40 @@
-package main
+package webfontsdk
 
 import (
 	"context"
 	"log"
+	"testing"
 	"time"
 
-	"github.com/webfont-io/webfont/sdk/golang/webfontsdk"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 )
 
-var conn *grpc.ClientConn
-
-func init() {
+func Test_get(t *testing.T) {
 	var err error
+	var conn *grpc.ClientConn
 	conn, err = grpc.Dial("localhost:5000", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	//defer conn.Close()
-	//c := webfontsdk.NewGreeterClient(conn)
-}
-
-func main() {
-	c := webfontsdk.NewGreeterClient(conn)
+	defer conn.Close()
+	c := NewGreeterClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	req := &webfontsdk.FontBuildRequest{
-		FontId:   12,
+	req := &FontBuildRequest{
+		FontId:   45803,
+		Apikey:   "798e7dac-bdd4-4049-b03c-7cce5832e013",
+		Text:     "English 中文字体测试",
 		NeedTtf:  true,
 		NeedEot:  true,
 		NeedWoff: true,
 	}
 	if ret, err := c.BuildFont(ctx, req); err != nil {
 		if actual, ok := status.FromError(err); ok {
-			log.Println("actual", "code", actual.Code(), "err:", actual.Message())
+			t.Fatal("actual", "code", actual.Code(), "err:", actual.Message())
 		}
 	} else {
-		if len(ret.GetBytesTtf()) > 0 {
-			//savettf
-		}
-
-		if len(ret.GetBytesEot()) > 0 {
-			//saveeot
-		}
-
-		if len(ret.GetBytesWoff()) > 0 {
-			//savewoff
-		}
+		t.Log("ttf", len(ret.GetBytesTtf()), "eot", len(ret.GetBytesEot()), "woff", len(ret.GetBytesWoff()))
 		//拼装font-familly
 	}
-
 }
