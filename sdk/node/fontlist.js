ec088f5fbb0e38d8210aef367f9f5b7867e82b33
hkloudou <hkloudou@gmail.com>
2019-03-11 05:53:36 +0800

    fix

--- sdk/node/fontlist.js
+++ sdk/node/fontlist.js
@@ -8,7 +8,10 @@ function fontlist() {
   var request = new messages.FontListRequest();
   request.setApikey("798e7dac-bdd4-4049-b03c-7cce5832e013")
   client.fontList(request, function(err, response) {
-    console.log("err", err);
+    if (err!=null) {
+      console.log("err Code", err.code, "detail:", err.details)
+      return;
+    }
     var maps = response.getFontsMap();
     console.log("maps",maps);
   });
