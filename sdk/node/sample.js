53a25651e562d3fbe76d5d8fdd42ae91983c1a17
hkloudou <hkloudou@gmail.com>
2019-03-11 04:33:54 +0800

    fix

--- sdk/node/sample.js
+++ sdk/node/sample.js
@@ -1,10 +1,10 @@
-var messages = require('./lib/webfont_pb.js');
-var services = require('./lib/webfont_grpc_pb.js');
+var messages = require('./lib/webfont_pb');
+var services = require('./lib/webfont_grpc_pb');
 
 var grpc = require('grpc');
+var client = new services.GreeterClient('localhost:5000',grpc.credentials.createInsecure());
 
-function main() {
-  var client = new services.GreeterClient('localhost:5000',grpc.credentials.createInsecure());
+function fontlist() {
   var request = new messages.FontListRequest();
   request.setApikey("798e7dac-bdd4-4049-b03c-7cce5832e013")
   client.fontList(request, function(err, response) {
@@ -13,4 +13,23 @@ function main() {
     console.log("maps",maps);
   });
 }
-main();
+
+
+function fontbuild() {
+  var request = new messages.FontBuildRequest();
+  request.setApikey("798e7dac-bdd4-4049-b03c-7cce5832e013");
+  request.setFontId(45803);
+  request.setText("中文字体");
+  request.setNeedTtf(true);
+  request.setNeedEot(false);
+  request.setNeedWoff(false);
+
+  client.buildFont(request, function(err, response) {
+    console.log("err", err);
+    console.log("ttf",response.getBytesTtf());
+  });
+}
+
+
+fontlist();
+fontbuild();
